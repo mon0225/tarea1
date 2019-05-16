2593fe5ff6fff8103e916347c8a590858d0b5325
mon0225 <elizabeta0225@hotmail.com>
2019-05-16 00:05:43 -0500

    casi intento

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -9,27 +9,32 @@ class App extends Component {
     super(props);
     this.state = {
       contacts: contacts.slice(0, 5),
-      randomContact: [],
       value: 0
     };
     this.handleClick = this.handleClick.bind(this);
-    this.addContact = this.addContact.bind(this);
+    //this.addContact = this.addContact.bind(this);
   }
 
-  handleClick(e){
-    let newContacts = [];
-    for( var i = 0; i < this.state.contacts.length; i++)
-    newContacts.push(contacts[i])
-    this.setState({
-        contacts: newContacts,
-    })
-    console.log(this.state)
+  handleClick(){
+
+      let newContacts = []
+    for( var i = 0; i < this.state.contacts.length; i++){
+      const result = (Math.floor(Math.random()* (contacts.length*4))
+      console.log(result)
+      newContacts.push(contacts.concat(result))
+      this.setState({
+        contacts: newContacts
+      })
     }
-  addContact(){
-    this.setState({contacts: (Math.floor(Math.random()*5)+1)});
+    
+
+    
+
+
+    
   }
 
-removeItem(index){
+/*removeItem(index){
   this.setState({
     items: this.state.contacts.filter((e, i)=>{
       return i !== index
@@ -42,22 +47,22 @@ removeItem(index){
       contacts.splice(index, 1);
       this.setState({contacts});
     }
- 
+ */
   render() {
     console.log(this.state.contacts)
-    console.log(this.state.randomContact)
+    
     return (
       <div className="App">
         <NavBar />
-        {this.state.contacts.map((artists, i, deleteItem, handleClick) => {
+        {this.state.contacts.map((artists, i) => {
           return(
           //console.log(artists.pictureUrl)
             <center>
               <Table key={i} name={artists.name} popularity={artists.popularity} picture={artists.pictureUrl} 
-              deleteItem={() => deleteItem(i)} key={i} {...artists}
+              removeItem={this.props.removeItem}
               />
-              <button onClick={this.handleClick}>{this.state.artists}</button>   
-              
+              <button onClick={this.handleClick}></button>   
+              <button addContact={this.addContact}>{this.state.artists}</button>   
             </center>
           )
           
